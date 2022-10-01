a9f7727f50965fda2776a31780683d6fdb664b0f
Dev-Bay.com <hello-contact@dev-bay.com>
2022-10-01 22:51:39 +0200

    first working version done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { hexagonsReducer } from './state/hexagons.reducer';
+import { layersReducer, poisReducer } from './state/map.reducer';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    StoreModule.forRoot({ hexagons: hexagonsReducer, layers: layersReducer, POIs: poisReducer }),
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
